872a45c01151d8c80a44b3ca248343cfba3f5929
Artur Dryomov <artur.dryomov@gmail.com>
2019-12-28 16:48:25 +0300

    Change the script for macOS since there is no CI.

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -5,7 +5,7 @@ HUGO_PACKAGE="hugo.tar.gz"
 HUGO_DIRECTORY="hugo"
 HUGO_BINARY="hugo"
 
-curl --location "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" --output "${HUGO_PACKAGE}"
+curl --location "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_macOS-64bit.tar.gz" --output "${HUGO_PACKAGE}"
 mkdir -p "${HUGO_DIRECTORY}" && tar -xzf "${HUGO_PACKAGE}" -C "${HUGO_DIRECTORY}"
 
 "./${HUGO_DIRECTORY}/${HUGO_BINARY}"
